bc6b0bd1091d8cbb370b80341be6981fa0a922c2
Daniel Aleksandersen <code@daniel.priv.no>
2017-05-08 01:51:45 +0200

    First WebExtension version

---
+++ background.js
@@ -0,0 +1,36 @@
+// Mozilla WebExtension or Chromium Extension
+if (typeof browser === 'undefined' && typeof chrome !== 'undefined') {
+  browser = chrome;
+}
+
+browser.webRequest.onBeforeSendHeaders.addListener(
+  function(details) {
+    var headers = details.requestHeaders;
+    
+    // Check if browser or another extension has already enabled the header
+    for (var index = 0; index < headers.length; ++index) {
+      if ('name' in headers[index] &&
+          headers[index].name.toLowerCase().indexOf('save-data') >= 0) {
+
+        // Return the original headers
+        return {
+          requestHeaders: details.requestHeaders
+    } } };
+
+    // Append the actual header
+    headers.push({
+      'name': 'Save-Data',
+      'value': 'on'
+    });
+
+    return {
+      requestHeaders: details.requestHeaders
+    };
+  }, {
+    urls: ["<all_urls>"]
+  }, [
+    "blocking",
+    "requestHeaders"
+  ]
+);
+
